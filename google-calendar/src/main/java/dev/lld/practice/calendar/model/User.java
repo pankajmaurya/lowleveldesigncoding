b5c2b6bf99029675d7a4054e587feced76b2a0d2
Pankaj Maurya <pankajmaurya@gmail.com>
2025-08-12 11:02:18 +0530

    missed changes

--- google-calendar/src/main/java/dev/lld/practice/calendar/model/User.java
+++
@@ -1,7 +0,0 @@
-package dev.lld.practice.calendar.model;
-
-/**
- * Created by gss on 10/08/25
- **/
-public class User {
-}
--- google-calendar/src/main/java/dev/lld/practice/calendar/service/NotificationService.java
+++ google-calendar/src/main/java/dev/lld/practice/calendar/service/NotificationService.java
@@ -4,5 +4,5 @@ package dev.lld.practice.calendar.service;
  * Created by gss on 10/08/25
  **/
 public interface NotificationService {
-    void sendNotification(String from, String to, String title, String body);
+    void sendNotification(String from, String to, String title, String eventId, String body);
 }
