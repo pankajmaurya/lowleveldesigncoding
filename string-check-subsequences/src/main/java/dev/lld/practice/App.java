58b37d5666b8c27f9788d0dfe8e73a9ea2342c36
Pankaj Maurya <pankajmaurya@gmail.com>
2025-09-26 16:38:54 +0530

    another problem

---
+++ string-check-subsequences/src/main/java/dev/lld/practice/App.java
@@ -0,0 +1,18 @@
+package dev.lld.practice;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Given a large string ('a'-'z' chars only) 'G'.
+ * And you are also given a large number of smaller string, S = {S1, S2, ... Sn}.
+ * Your task is to find out which of these S_i are subsequence of 'G'.
+ * Limits: |G| < 10^9, |S| = 10^7, |S_i| <= 100
+ * Subequence: A string 'X' is a subsequence of string 'Y', if you can remove zero or more characters from 'Y' and get 'X'.
+ */
+public class App 
+{
+    public List<String> getSubsequences(String G, List<String> S) {
+        return null;
+    }
+}
